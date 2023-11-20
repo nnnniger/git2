f2b2de7ba5348dca887779bf6338afd87ee584af
Viktor <vikt0r.sabanov@yandex.ru>
2023-11-20 19:26:57 +0300

    finish version

--- main2.py
+++ main2.py
@@ -1,47 +1,33 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QGraphicsScene, QGraphicsView, \
-    QGraphicsEllipseItem
-from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtCore import Qt
+import io
 import random
+import sys
+
+from PyQt5 import uic, QtWidgets, Qt
+from PyQt5.QtWidgets import QApplication, QGraphicsEllipseItem
 
 
-class CircleWidget(QWidget):
+class MyWidget(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        self.initUI()
+        uic.loadUi('UI.ui', self)
 
-    def initUI(self):
         self.setGeometry(300, 300, 400, 300)
-        self.setWindowTitle('Random Circles')
-
-        self.scene = QGraphicsScene(self)
-        self.view = QGraphicsView(self.scene)
-        self.layout = QVBoxLayout(self)
-        self.layout.addWidget(self.view)
-
-        self.addButton = QPushButton('добавить жёлтый круг', self)
-        self.addButton.clicked.connect(self.addCircle)
-        self.layout.addWidget(self.addButton)
+        self.setWindowTitle('ЖЕЛТЫЕ КРУЖКИ')
+        self.scene = QtWidgets.QGraphicsScene(self)
+        self.view.setScene(self.scene)
+        self.newcircleButton.clicked.connect(self.newCircle)
 
     def newCircle(self):
         diameter = random.randint(10, 100)
         x = random.randint(0, self.width() - diameter)
         y = random.randint(0, self.height() - diameter)
-
-        # color = QColor(Qt.yellow)
-        #
-        # self.scene.addEllipse(x, y, diameter, diameter)
-        # self.scene.setBrush(color)
-        color = QColor(Qt.yellow)
         ellipse = QGraphicsEllipseItem(x, y, diameter, diameter)
-        ellipse.setBrush(color)
+        ellipse.setBrush(Qt.Qt.yellow)
         self.scene.addItem(ellipse)
-
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = CircleWidget()
+    ex = MyWidget()
     ex.show()
     sys.exit(app.exec_())
